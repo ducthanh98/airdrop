413d68c83cb68b88835db314521467cda93b3b4f
thanhld <thanhld9x.dev@gmail.com>
2024-06-25 11:12:46 +0700

    nodepay: add proxy

--- nodepay/main.go
+++ nodepay/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"github.com/go-resty/resty/v2"
@@ -8,6 +10,9 @@ import (
 	"github.com/gorilla/websocket"
 	"github.com/spf13/viper"
 	"log"
+	"net"
+	"net/http"
+	"net/url"
 	"nodepay/constant"
 	"nodepay/request"
 	"time"
@@ -21,29 +26,108 @@ func main() {
 	}
 
 	token := viper.GetString("data.token")
+	proxies := viper.GetStringSlice("data.proxies")
 
-	client := resty.New()
-	var publicIp *request.GetIPResponse
+	for _, proxyURL := range proxies {
+		client := resty.New()
+		if proxyURL != "" {
+			client.SetProxy(proxyURL)
+		}
+		var publicIp *request.GetIPResponse
 
-	_, err = client.R().
-		SetResult(&publicIp).
-		Get("https://api.ipify.org?format=json")
-	if err != nil {
-		panic("Can't get public ip")
+		_, err = client.R().
+			SetResult(&publicIp).
+			Get("https://api.ipify.org?format=json")
+		if err != nil {
+			panic("Can't get public ip")
+		}
+
+		go pingNetworkDevice(token, proxyURL, publicIp.IP)
+
+		go connectSocket(token, proxyURL, publicIp.IP)
 	}
 
-	go pingNetworkDevice(token, publicIp.IP)
-	go connectSocket(token)
 	select {}
 
 }
 
-func connectSocket(token string) {
-	c, resp, err := websocket.DefaultDialer.Dial(constant.BASE_WSS, nil)
-	if err != nil {
-		log.Printf("handshake failed with status %d", resp.StatusCode)
-		log.Fatal("dial:", err)
+func connectSocket(token string, proxyURL string, ip string) {
+	client := resty.New()
+	if proxyURL != "" {
+		client.SetProxy(proxyURL)
 	}
+	var authSessionResponse *request.AuthSessionResponse
+
+	_, err := client.R().
+		SetResult(&authSessionResponse).
+		SetAuthToken(token).
+		Post(fmt.Sprintf("%v/auth/session", constant.BASE_URL))
+	if err != nil {
+		panic(err)
+	}
+
+	var c *websocket.Conn
+
+	if proxyURL != "" {
+		proxy, err := url.Parse(proxyURL)
+		if err != nil {
+			panic(err)
+		}
+
+		ws, err := url.Parse(constant.BASE_WSS)
+		if err != nil {
+			panic(err)
+		}
+
+		conn, err := net.Dial("tcp", proxy.Host)
+		if err != nil {
+			panic(err)
+		}
+		defer conn.Close()
+
+		connectReq := &http.Request{
+			Method: http.MethodConnect,
+			URL:    proxy,
+			Host:   ws.Host,
+			Header: make(http.Header),
+		}
+		connectReq.Header.Set("Proxy-Connection", "keep-alive")
+
+		pass, _ := proxy.User.Password()
+
+		auth := proxy.User.Username() + ":" + pass
+		basicAuth := "Basic " + base64.StdEncoding.EncodeToString([]byte(auth))
+		connectReq.Header.Set("Proxy-Authorization", basicAuth)
+
+		err = connectReq.Write(conn)
+		if err != nil {
+			panic(err)
+		}
+
+		resp, err := http.ReadResponse(bufio.NewReader(conn), connectReq)
+		if err != nil {
+			panic(err)
+		}
+		if resp.StatusCode != http.StatusOK {
+			panic(fmt.Errorf("Proxy CONNECT failed with status: %s", resp.Status))
+		}
+
+		// Use the resulting connection as a WebSocket connection
+		wsCon, resp, err := websocket.NewClient(conn, ws, nil, 1024, 1024)
+		if err != nil {
+			log.Printf("Init ws failed with status %d", resp.StatusCode)
+			log.Fatal("dial:", err)
+		}
+		c = wsCon
+	} else {
+		wsCon, resp, err := websocket.DefaultDialer.Dial(constant.BASE_WSS, nil)
+		if err != nil {
+			log.Printf("Init ws failed with status %d", resp.StatusCode)
+			log.Fatal("dial:", err)
+		}
+		c = wsCon
+	}
+
 	defer c.Close()
 	for {
 		_, payload, err := c.ReadMessage()
@@ -51,14 +135,14 @@ func connectSocket(token string) {
 			log.Println("Read message error:", err)
 			time.Sleep(1 * time.Minute)
 			log.Println("Error. Reconnecting after a minute")
-			go connectSocket(token)
+			go connectSocket(token, proxyURL, ip)
 			return
 		}
 		var msg *request.WsMessage
 		json.Unmarshal(payload, &msg)
-		log.Printf("recv: %s", msg)
+		log.Printf("%v: recv: %s", ip, msg)
 		if msg.Action == "AUTH" {
-			sendAuth(msg, token, c)
+			sendAuth(msg, token, c, authSessionResponse)
 		} else if msg.Action == "PONG" {
 			SendPong(msg, c)
 			time.Sleep(constant.PING_INTERVAL * time.Millisecond)
@@ -93,11 +177,11 @@ func SendPing(msg *request.WsMessage, c *websocket.Conn) {
 	}
 }
 
-func sendAuth(msg *request.WsMessage, token string, c *websocket.Conn) {
+func sendAuth(msg *request.WsMessage, token string, c *websocket.Conn, authSessionResponse *request.AuthSessionResponse) {
 	tmp := request.AuthMessage{
 		ID:           msg.ID,
 		Action:       "PING",
-		UserID:       "1245581001883648000",
+		UserID:       authSessionResponse.Data.UID,
 		BrowserID:    uuid.New().String(),
 		UserAgent:    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
 		Timestamp:    time.Now().Format("20060102150405"),
@@ -114,8 +198,11 @@ func sendAuth(msg *request.WsMessage, token string, c *websocket.Conn) {
 	}
 }
 
-func pingNetworkDevice(token, ip string) {
+func pingNetworkDevice(token, proxyURL string, ip string) {
 	client := resty.New()
+	if proxyURL != "" {
+		client.SetProxy(proxyURL)
+	}
 
 	for {
 
@@ -126,8 +213,6 @@ func pingNetworkDevice(token, ip string) {
 			log.Println("Can't ping device")
 		}
 
-		//fmt.Println("res", res)
-
-		time.Sleep(1 * time.Second)
+		time.Sleep(3 * time.Second)
 	}
 }
--- nodepay/request/response.go
+++ nodepay/request/response.go
@@ -8,3 +8,25 @@ type WsMessage struct {
 	ID     string `json:"id"`
 	Action string `json:"action"`
 }
+
+type AuthSessionResponse struct {
+	Success bool            `json:"success"`
+	Code    int             `json:"code"`
+	Msg     string          `json:"msg"`
+	Data    AuthSessionData `json:"data"`
+}
+type Balance struct {
+	CurrentAmount  float64 `json:"current_amount"`
+	TotalCollected float64 `json:"total_collected"`
+	TotalRedeemed  float64 `json:"total_redeemed"`
+}
+type AuthSessionData struct {
+	UID          string  `json:"uid"`
+	Name         string  `json:"name"`
+	Email        string  `json:"email"`
+	Avatar       string  `json:"avatar"`
+	ReferralCode string  `json:"referral_code"`
+	State        string  `json:"state"`
+	ReferralLink string  `json:"referral_link"`
+	Balance      Balance `json:"balance"`
+}
