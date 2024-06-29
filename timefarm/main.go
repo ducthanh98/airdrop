e3e23abe6dabd83d09b0232de03c823be0bd6d58
thanhld <thanhld9x.dev@gmail.com>
2024-06-30 01:07:46 +0700

    timefarm: auto complete task

--- timefarm/main.go
+++ timefarm/main.go
@@ -24,84 +24,8 @@ func main() {
 
 		go collect(token, proxy)
 	}
-	go checkTask(tokens, proxies)
 
 	select {}
-
-}
-
-func checkTask(queries, proxies []string) {
-
-	for {
-
-		for i, query := range queries {
-			proxy := proxies[i%len(proxies)]
-
-			client := resty.New().SetProxy(proxy)
-
-			var authResponse request.AuthResponse
-			res, err := client.
-				SetProxy(proxy).
-				R().
-				SetBody(query).
-				SetResult(&authResponse).
-				Post(constant.AuthAPI)
-			if err != nil {
-				fmt.Println("Account idx:", i, "Get auth err: ", err)
-				continue
-			}
-
-			token := authResponse.Token
-			var tasks request.Task
-
-			res, err = client.
-				R().
-				SetAuthToken(token).
-				SetBody(`{}`).
-				SetResult(&tasks).
-				Get(constant.GetTaskAPI)
-			if err != nil {
-				fmt.Println("Get task err: ", err)
-			}
-
-			for _, task := range tasks {
-				if task.Submission == nil {
-					res, err = client.
-						R().
-						SetAuthToken(token).
-						SetBody(`{}`).
-						SetResult(&tasks).
-						Post(fmt.Sprintf(constant.SubmitTaskAPI, task.ID))
-					if err != nil {
-						fmt.Println("Get task err: ", err)
-					}
-					fmt.Println("Account idx", i, "finish: ", task.ID, res)
-				} else if task.Submission.Status == constant.TaskStatusComplete {
-					res, err = client.
-						R().
-						SetAuthToken(token).
-						SetBody(`{}`).
-						SetResult(&tasks).
-						Post(fmt.Sprintf(constant.ClaimTaskAPI, task.ID))
-					fmt.Println("Account idx", i, "claim: ", task.ID, res)
-
-				}
-			}
-			time.Sleep(10 * time.Second)
-
-			res, err = client.
-				R().
-				SetAuthToken(token).
-				SetBody(`{}`).
-				SetResult(&tasks).
-				Post(fmt.Sprintf(constant.UpgradeTimeAPI))
-			fmt.Println("Account idx", i, "try to upgrade time: ", res)
-			time.Sleep(20 * time.Second)
-		}
-
-		fmt.Println("All done. Wait 2 hours")
-		time.Sleep(2 * time.Hour)
-	}
 
 }
 
@@ -149,7 +73,7 @@ func collect(query string, proxy string) {
 			return
 		}
 		fmt.Println("Start api", res)
-		time.Sleep(5 * time.Minute)
+		time.Sleep(4*time.Hour + 10*time.Minute)
 	}
 
 }
---
+++ timefarm/upgrade/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-resty/resty/v2"
+	"github.com/spf13/viper"
+	"timefarm/constant"
+	"timefarm/request"
+)
+
+func main() {
+	viper.SetConfigFile("./conf.toml")
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
+	tokens := viper.GetStringSlice("data.tokens")
+	proxies := viper.GetStringSlice("proxies.data")
+	checkTask(tokens, proxies)
+
+}
+
+func checkTask(queries, proxies []string) {
+
+	for {
+
+		for i, query := range queries {
+			proxy := proxies[i%len(proxies)]
+
+			client := resty.New().SetProxy(proxy)
+
+			var authResponse request.AuthResponse
+			res, err := client.
+				SetProxy(proxy).
+				R().
+				SetBody(query).
+				SetResult(&authResponse).
+				Post(constant.AuthAPI)
+			if err != nil {
+				fmt.Println("Account idx:", i, "Get auth err: ", err)
+				continue
+			}
+
+			token := authResponse.Token
+			var tasks request.Task
+
+			res, err = client.
+				R().
+				SetAuthToken(token).
+				SetBody(`{}`).
+				SetResult(&tasks).
+				Get(constant.GetTaskAPI)
+			if err != nil {
+				fmt.Println("Get task err: ", err)
+			}
+
+			for _, task := range tasks {
+				if task.Submission == nil {
+					res, err = client.
+						R().
+						SetAuthToken(token).
+						SetBody(`{}`).
+						SetResult(&tasks).
+						Post(fmt.Sprintf(constant.SubmitTaskAPI, task.ID))
+					if err != nil {
+						fmt.Println("Get task err: ", err)
+					}
+					fmt.Println("Account idx", i, "finish: ", task.ID, res)
+				} else if task.Submission.Status == constant.TaskStatusComplete {
+					res, err = client.
+						R().
+						SetAuthToken(token).
+						SetBody(`{}`).
+						SetResult(&tasks).
+						Post(fmt.Sprintf(constant.ClaimTaskAPI, task.ID))
+					fmt.Println("Account idx", i, "claim: ", task.ID, res)
+
+				}
+			}
+
+			res, err = client.
+				R().
+				SetAuthToken(token).
+				SetBody(`{}`).
+				SetResult(&tasks).
+				Post(fmt.Sprintf(constant.UpgradeTimeAPI))
+			fmt.Println("Account idx", i, "try to upgrade time: ", res)
+		}
+
+		fmt.Println("All done. Wait 2 hours")
+	}
+
+}
