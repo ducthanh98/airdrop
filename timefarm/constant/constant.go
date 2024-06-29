498ca4fdae75d976f7148f3652e1fe973168836c
thanhld <thanhld9x.dev@gmail.com>
2024-06-30 00:40:41 +0700

    timefarm: auto complete task

--- timefarm/constant/constant.go
+++ timefarm/constant/constant.go
@@ -9,7 +9,7 @@ var FinishAPI = fmt.Sprintf("%v/farming/finish", BASE_URL)
 var StartAPI = fmt.Sprintf("%v/farming/start", BASE_URL)
 var GetTaskAPI = fmt.Sprintf("%v/tasks", BASE_URL)
 var SubmitTaskAPI = fmt.Sprintf("%v/tasks/%v/submissions", BASE_URL, "%v")
-var ClaimTaskAPI = fmt.Sprintf("%v/tasks/%v/submissions", BASE_URL, "%v")
+var ClaimTaskAPI = fmt.Sprintf("%v/tasks/%v/claims", BASE_URL, "%v")
 
 const (
 	TaskStatusComplete = "COMPLETED"
