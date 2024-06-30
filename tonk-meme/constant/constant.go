e7045702e50e004761278cb540bca1c8e11780e9
thanhld <thanhld9x.dev@gmail.com>
2024-06-30 14:31:06 +0700

    tonk: auto

---
+++ tonk-meme/constant/constant.go
@@ -0,0 +1,5 @@
+package constant
+
+const BASE_URL = "https://backend-v2.tonk.meme/miniapp-backend"
+
+const MinedToken = BASE_URL + "/transactions/add_new_transaction.php"
---
+++ tonk-meme/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-resty/resty/v2"
+	"github.com/spf13/cast"
+	"github.com/spf13/viper"
+	"math/rand"
+	"time"
+	"tonk/constant"
+)
+
+func main() {
+	viper.SetConfigFile("./conf.toml")
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
+	tokens := viper.GetStringSlice("auth.tokens")
+	proxies := viper.GetStringSlice("proxies.data")
+	for i, token := range tokens {
+		proxy := proxies[i%len(proxies)]
+		go collect(token, proxy)
+
+	}
+	select {}
+}
+
+func collect(token, proxy string) {
+	client := resty.New().SetProxy(proxy)
+	for {
+		rand.Seed(time.Now().UnixNano())
+
+		// Generate a random number between 100 and 500
+		min := 100
+		max := 500
+		amount := rand.Intn(max-min+1) + min
+
+		resp, err := client.R().
+			SetBody(fmt.Sprintf(`{"user_id":%d,"amount":%d,"type":"credit","description":"Deposit"}`, cast.ToInt(token), amount)).
+			Post(constant.MinedToken)
+		if err != nil {
+			fmt.Println("Mine ", amount, "failed", err)
+		}
+		fmt.Println("Mine ", amount, "success: ", resp)
+		time.Sleep(time.Minute)
+
+	}
+}
