b6c42798c3df159e1aeb7586d093f56f70535ec2
thanhld <thanhld9x.dev@gmail.com>
2024-07-05 14:33:32 +0700

    rm oxygen

--- oxygen-miner/constant/constant.go
+++
@@ -1,5 +0,0 @@
-package constant
-
-const BASE_URL = "https://oxygenminer.online"
-
-const FARM_URL = BASE_URL + "/farm"
--- oxygen-miner/request/request.go
+++
@@ -1,6 +0,0 @@
-package request
-
-type FarmRequest struct {
-	Hash string `json:"hash"`
-	Tg   string `json:"tg"` // 1939627995
-}
